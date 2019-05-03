6b34b47a3d48d88e30fc77af5bf8f4797106ae1d
rickyrsp <30280402+rickyrsp@users.noreply.github.com>
2019-05-03 17:15:19 +0700

    Add files via upload

---
+++ copyright_ricky.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "http://www.bungfrangki.com/") {
+        window.location.href = "https://www.trikricky.com"; }});
+//]]>
+</script>
